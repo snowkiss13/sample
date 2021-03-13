4ddea72a399f0f39fc1cfd5d87220417828f30bf
snowkiss13 <snowkiss13@gmail.com>
2021-03-14 01:05:46 +0900

    マスアサインメントとStrong Parametersを作成した。

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -5,9 +5,8 @@ class BlogsController < ApplicationController
     @blog = Blog.new
   end
   def create
-    Blog.create(title: params[:blog][:title], content: params[:blog][:content])
+    Blog.create(params.require(:blog).permit(:title, :content))
     # new_blog_pathというPrefixを書くことで、"/blogs/new"というURLの指定をします、という意味になる。
     redirect_to new_blog_path
-  end
   end
 end
