be1973cdcc79af4d9a7dd38e38370bd9e8c253d1
snowkiss13 <snowkiss13@gmail.com>
2021-03-14 08:15:29 +0900

    ブログ一覧画面を作成

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,5 +1,7 @@
 class BlogsController < ApplicationController
   def index
+    # 全てのブログを取得する命令
+    @blogs = Blog.all
   end
   def new
     @blog = Blog.new
@@ -8,5 +10,9 @@ class BlogsController < ApplicationController
     Blog.create(params.require(:blog).permit(:title, :content))
     # new_blog_pathというPrefixを書くことで、"/blogs/new"というURLの指定をします、という意味になる。
     redirect_to new_blog_path
+  end
+  private
+  def blog_params
+    params.require(:blog).permit(:title, :content)
   end
 end
